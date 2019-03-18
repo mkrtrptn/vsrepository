146fcb93089cba71707c954c74a55118863256a1
mktrptn <mktr@gmail.com>
2019-03-18 20:24:33 +0530

    DemoController added

---
+++ vsgitmvcapp/vsgitmvcapp/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vsgitmvcapp.Controllers
+{
+    public class DemoController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
